7224136face9e08ca2851795cd266189a6582a91
FrancescoSls97 <frankds1997@gmail.com>
2025-07-23 19:33:09 +0200

    error prevent default

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   //costanti per le funzioni
   const [author, setAuthor] = useState('')
   const [title, setTitle] = useState('')
   const [message, setMessage] = useState ('')
-  const [send, setSend] = useState(false)
+  
+
+  //API
+  const url = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
+  
+//fetch function
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const requestOptions = {
+    method: 'POST',
+    header: {
+      'Connect-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      author: author,
+      title: title,
+      message: message,
+    })
+  };
+    fetch(url, requestOptions)
+    .then (response => response.json())
+    .then (data => {
+      console.log('Hey!! Listen!!');
+      console.log(data);     
+    })   
+};
 
 //funzione per settare il nome dell'autore
   function handleAuthorName(e) {
@@ -25,15 +51,13 @@ function App() {
     setMessage(e.target.value) 
   }
 
-  function handleSend(e) {
-    console.log(e.target.name)
-    setSend(e.target.checked)
-  }
-  
+
 
   return (
     <>
     <div className="container m-5">
+      <form action={handleSubmit}>
+
       <div className="card m-3 p-3">
         <h1>Send Something To Boolean</h1>
           <div className="mb-3">
@@ -67,11 +91,12 @@ function App() {
             <textarea className="form-control p-3" name="message" id="message" rows="3" placeholder='Write your message here' value={message} onChange={handleMessage}></textarea>
           </div>
           <div className="form-check mb-2">
-            <input className="form-check-input" type="checkbox" name="send" id="send" onChange={handleSend}/>
+            <input className="form-check-input" type="checkbox" name="send" id="send" />
             <label className="form-check-label" htmlFor="send"> Send </label>
           </div>
           <button type='submit' className='btn btn-primary p-3 fs-4'> Send your post <i className="fa-regular fa-message"></i> </button>        
       </div>
+      </form>
     </div>
     </>
   )
