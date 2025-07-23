17b532b77930fc0f1ca44dfe8d17c1f9ebdd9018
FrancescoSls97 <frankds1997@gmail.com>
2025-07-23 15:43:19 +0200

    set function for form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import { useState } from 'react'
 
 function App() {
+  const [author, setAuthor] = useState('')
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState ('')
+
+  function handleAuthorName(e) {
+    console.log(e.target.name);
+    setAuthor(e.target.value) 
+  }
+
+  function handleTitleName(e) {
+    console.log(e.target.name);
+    setTitle(e.target.value) 
+  }
+
+  function handleMessage(e) {
+    console.log(e.target.name);
+    setMessage(e.target.value) 
+  }
   
 
   return (
@@ -9,7 +27,7 @@ function App() {
       <div className="card m-3 p-3">
         <h1>Send Something To Boolean</h1>
           <div className="mb-3">
-            <label for="author" className="form-label fs-4">Author</label>
+            <label forhtml="author" className="form-label fs-4">Author</label>
             <input
               type="text"
               className="form-control p-3"
@@ -17,10 +35,12 @@ function App() {
               id="author"
               aria-describedby="helpId"
               placeholder="Author Name"
+              value={author}
+              onChange={handleAuthorName}
             />
           </div>
           <div className="mb-3">
-            <label for="title" className="form-label fs-4">Title</label>
+            <label forhtml="title" className="form-label fs-4">Title</label>
             <input
               type="text"
               className="form-control p-3"
@@ -28,13 +48,15 @@ function App() {
               id="title"
               aria-describedby="helpId"
               placeholder="Post Title"
+              value={title}
+              onChange={handleTitleName}
             />
           </div>
-          <div class="mb-3">
-            <label for="message" class="form-label fs-4">Your message</label>
-            <textarea class="form-control p-3" name="message" id="message" rows="3" placeholder='Write your message here'></textarea>
+          <div className="mb-3">
+            <label forhtml="message" className="form-label fs-4">Your message</label>
+            <textarea className="form-control p-3" name="message" id="message" rows="3" placeholder='Write your message here' value={message} onChange={handleMessage}></textarea>
           </div>
-          <button type='submit' className='btn btn-primary p-3 fs-4'> Send your message <i class="fa-regular fa-message"></i> </button>        
+          <button type='submit' className='btn btn-primary p-3 fs-4'> Send your message <i className="fa-regular fa-message"></i> </button>        
       </div>
     </div>
     </>
